c365319c858a2d70b584a2526842880a9d860b3c
Harshada Popalkar <123169976+harshada-popalkar@users.noreply.github.com>
2023-05-27 14:51:47 +0530

    Update DATE_formate.sql

--- DATE_formate.sql
+++ DATE_formate.sql
@@ -6,12 +6,12 @@ SELECT DATE_FORMAT(CURDATE() ,'%m/%d/%Y') TODAY;
 
 --- create table
 CREATE TABLE sales_orders(
-id INTEGER ,
-customer_id INTEGER ,
-order_date DATE ,
-financialcode CHAR(2) ,
-region CHAR(7) ,
-sales_representative INTEGER
+	id INTEGER ,
+	customer_id INTEGER ,
+	order_date DATE ,
+	financialcode CHAR(2) ,
+	region CHAR(7) ,
+	sales_representative INTEGER
 );
 
 DROP TABLE sales_orders ;
@@ -20,33 +20,33 @@ INSERT INTO sales_orders(id ,customer_id ,order_date,financialcode,region,sales_
 	VALUES
 	(2001,101,'2000-03-16' ,'r1','eastern',299) ,
 	(2002,102,'2000-03-17' ,'r2' ,'western',399) ,
-    (2003,103,'2000-03-18' , 'r3', 'western',499),
-    (2004,104,'2000-03-19' ,'r4' ,'eastern',599),
-    (2005,105,'2000-03-20' , 'r5' ,'eastern',699)
+        (2003,103,'2000-03-18' , 'r3', 'western',499),
+        (2004,104,'2000-03-19' ,'r4' ,'eastern',599),
+        (2005,105,'2000-03-20' , 'r5' ,'eastern',699)
 ;
 
 SELECT * FROM sales_orders ;
 
 SELECT * FROM sales_orders 
 	WHERE order_date 
-    BETWEEN '2000-03-15' AND '2000-03-20' ;
+        BETWEEN '2000-03-15' AND '2000-03-20' ;
     
 SELECT
 	EXTRACT(YEAR FROM order_date) AS order_year ,
-    EXTRACT(MONTH FROM order_date) AS order_month ,
-    EXTRACT(DAY FROM order_date) AS order_date,
-    customer_id
-    FROM sales_orders
-    WHERE customer_id BETWEEN 102 AND 105
+        EXTRACT(MONTH FROM order_date) AS order_month ,
+        EXTRACT(DAY FROM order_date) AS order_date,
+        customer_id
+        FROM sales_orders
+        WHERE customer_id BETWEEN 102 AND 105
     ;
     
 SELECT * FROM sales_orders 
-		WHERE YEAR(order_date) =2000 
-        AND customer_id = 102 ;
+         WHERE YEAR(order_date) =2000 
+         AND customer_id = 102 ;
 
 SELECT DATE_FORMAT
 	(STR_TO_DATE(order_date ,'%Y-%m-%d'),'%d-%m-%Y') 
-    AS date_format FROM sales_orders ;
+         AS date_format FROM sales_orders ;
 	
     
 
